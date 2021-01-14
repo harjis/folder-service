096975598a26c8d7edf2a10077b3b9ec0f30ff77
Joonas Harjukallio <harjukallio@gmail.com>
2021-01-14 14:56:57 +0200

    Complain if variables are not there

--- build_and_push_images.sh
+++ build_and_push_images.sh
@@ -1,5 +1,19 @@
+#!/bin/bash
+
+if [ -z "$VERSION" ]
+then
+      echo "\$VERSION is empty"
+      exit 1
+fi
+
+if [ -z "$SHA" ]
+then
+      echo "\$SHA is empty. Run SHA=\$(git rev-parse HEAD)"
+      exit 1
+fi
+
 docker build -t d0rka/folder-service-backend:latest -t d0rka/folder-service-backend:$SHA -t d0rka/folder-service-backend:$VERSION -f ./backend/Dockerfile.dev ./backend
-docker build -t d0rka/folder-service-frontend:latest -t d0rka/folder-service-frontend:$SHA -t d0rka/folder-service-backend:$VERSION -f ./frontend/Dockerfile.dev ./frontend
+docker build -t d0rka/folder-service-frontend:latest -t d0rka/folder-service-frontend:$SHA -t d0rka/folder-service-frontend:$VERSION -f ./frontend/Dockerfile.dev ./frontend
 
 docker push d0rka/folder-service-backend:latest
 docker push d0rka/folder-service-frontend:latest
